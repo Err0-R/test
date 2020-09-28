28afef083c5188db9f4297d1e8bec853976acf00
Err0-R <71987359+Err0-R@users.noreply.github.com>
2020-09-28 20:11:52 +0300

    test
    Test

---
+++ test.py
@@ -0,0 +1,27 @@
+import os
+os.system("clear")
+
+                                                                                                                     
+print("TTTTTTTTTTTTTTTTTTTTTTT                                          tttt    ")      
+print("T:::::::::::::::::::::T                                       ttt:::t     ")     
+print("T:::::::::::::::::::::T                                       t:::::t     ")     
+print("T:::::TT:::::::TT:::::T                                       t:::::t       ")   
+print("TTTTTT  T:::::T  TTTTTTeeeeeeeeeeee        ssssssssss   ttttttt:::::ttttttt   ") 
+print("        T:::::T      ee::::::::::::ee    ss::::::::::s  t:::::::::::::::::t   ") 
+print("        T:::::T     e::::::eeeee:::::eess:::::::::::::s t:::::::::::::::::t   ") 
+print("        T:::::T    e::::::e     e:::::es::::::ssss:::::stttttt:::::::tttttt  ")  
+print("        T:::::T    e:::::::eeeee::::::e s:::::s  ssssss       t:::::t         ") 
+print("        T:::::T    e:::::::::::::::::e    s::::::s            t:::::t ")         
+print("        T:::::T    e::::::eeeeeeeeeee        s::::::s         t:::::t    ")      
+print("        T:::::T    e:::::::e           ssssss   s:::::s       t:::::t    tttttt")
+print("      TT:::::::TT  e::::::::e          s:::::ssss::::::s      t::::::tttt:::::t")
+print("      T:::::::::T   e::::::::eeeeeeee  s::::::::::::::s       tt::::::::::::::t")
+print("      T:::::::::T    ee:::::::::::::e   s:::::::::::ss          tt:::::::::::tt")
+print("      TTTTTTTTTTT      eeeeeeeeeeeeee    sssssssssss              ttttttttttt  ")
+print("")
+print("")
+print("1 : Loop")
+
+x = input("type number: ")      
+if x == "1" :
+    os.system("git  clone  https://github.com/Err0-R/Loop.git")
